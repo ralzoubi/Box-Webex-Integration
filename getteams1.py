e8d97675f78d8613d7f67a541c4fc2feefafe70a
Ruqia Alzoubi <48746457+ralzoubi@users.noreply.github.com>
2021-06-21 13:27:57 -0500

    Update getteams1.py

--- getteams1.py
+++ getteams1.py
@@ -4,7 +4,7 @@ url = "https://webexapis.com/v1/memberships"
 
 payload={}
 headers = {
-  'Authorization': 'Bearer NWE4MDM2ZjAtMWJhMi00NDQzLWFlZDEtY2QzODBiOTc2NTY0OGYzMWQyOWYtYTkz_P0A1_6e31fd08-63c5-45b0-9a66-10c03c54016e'
+  'Authorization': 'Bearer '
 }
 
 response = requests.request("GET", url, headers=headers, data=payload)
